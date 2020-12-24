88526f39411b1576d22838c0f52c7cdf549cf84e
Thanh Nguyen <vnscriptkid@gmail.com>
2020-12-24 22:31:32 +0700

    Add model entities for Company and Employee

---
+++ Entities/Models/Company.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Models
+{
+    class Company
+    {
+        [Column("CompanyId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Company name is a required field.")]
+        [MaxLength(ErrorMessage = "Maximum length for the company name is 60 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Company address is a required field.")]
+        [MaxLength(ErrorMessage = "Maximum length for the company name is 60 characters.")]
+        public string Address { get; set; }
+
+        public string Country { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
---
+++ Entities/Models/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Entities.Models
+{
+    class Employee
+    {
+        [Column("EmployeeId")]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Employee name is a required field.")]
+        [MaxLength(ErrorMessage = "Maximum length for employee name is 30 characters.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Employee age is a required field.")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Employee position is a required field.")]
+        [MaxLength(ErrorMessage = "Maximum length for employee position is 20 characters.")]
+        public string Position { get; set; }
+
+        [ForeignKey(nameof(Company))]
+        public Guid CompanyId { get; set; }
+
+        public Company Company { get; set; }
+    }
+}
