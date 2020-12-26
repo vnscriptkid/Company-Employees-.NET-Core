54ce1beafad547e78a5f972b3a3fb77892b79e95
Thanh Nguyen <vnscriptkid@gmail.com>
2020-12-26 09:33:16 +0700

    Add GetAllCompanies endpoint

--- Contracts/ICompanyRepository.cs
+++ Contracts/ICompanyRepository.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using Entities.Models;
 
 namespace Contracts
 {
     public interface ICompanyRepository
     {
+        IEnumerable<Company> GetAllCompanies(bool trackChanges);
     }
 }
--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -3,6 +3,7 @@ using Entities;
 using Entities.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Repository
@@ -11,6 +12,13 @@ namespace Repository
     {
         public CompanyRepository(RepositoryContext repositoryContext) : base(repositoryContext)
         {
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            return FindAll(trackChanges)
+                .OrderBy(c => c.Name)
+                .ToList();
         }
     }
 }
